495869803a5cf7c863e654a25c89242dc5f4c225
hassan2282 <taghavey.hassan@gmail.com>
2025-11-14 20:21:23 +0330

    add role faker

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Role;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class RolesSeeder extends Seeder
@@ -13,6 +12,6 @@ class RolesSeeder extends Seeder
      */
     public function run(): void
     {
-//        Role::factory()->count(20)->create();
+        Role::factory()->count(20)->create();
     }
 }
