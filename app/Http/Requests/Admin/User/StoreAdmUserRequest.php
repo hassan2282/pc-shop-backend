ffa18c85f37c98e408c1d8ada30857bb58a97f9c
hassan2282 <taghavey.hassan@gmail.com>
2025-11-14 04:32:54 +0330

    edit AdmUserRequests name

--- app/Http/Requests/Admin/User/StoreAdmUserRequest.php
+++ app/Http/Requests/Admin/User/StoreAdmUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Admin\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AdmCreateUserRequest extends FormRequest
+class StoreAdmUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
