6cdff9135f26308296f9499110981aa01baa374c
hassan2282 <taghavey.hassan@gmail.com>
2025-11-17 03:19:14 +0330

    add articles table

---
+++ database/migrations/2025_11_16_202900_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('description');
+            $table->longText('text');
+            $table->integer('views');
+            $table->boolean('status')->default(1)->comment('0 => not published, 1 => published');
+            $table->foreignId('author_id')->constrained('users');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
