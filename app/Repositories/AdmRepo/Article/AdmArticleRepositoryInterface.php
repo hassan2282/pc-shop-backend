9bf450d55155f4ed4f752d961a39156f1b7fdfd2
hassan2282 <taghavey.hassan@gmail.com>
2025-11-17 03:19:53 +0330

    add article repository interface

---
+++ app/Repositories/AdmRepo/Article/AdmArticleRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\AdmRepo\Article;
+
+use App\Repositories\EloquentRepositoryInterface;
+
+interface AdmArticleRepositoryInterface extends EloquentRepositoryInterface
+{
+
+}
