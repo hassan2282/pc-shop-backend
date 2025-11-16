8b4c321694ea61b01b167e53a0bfc7eb314d93c2
hassan2282 <taghavey.hassan@gmail.com>
2025-11-17 03:22:57 +0330

    add article store validations

---
+++ app/Http/Requests/Admin/Article/StoreArticleRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests\Admin\Article;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|min:5|max:150',
+            'description' => 'required|string|min:20|max:500',
+            'text' => 'required|string|min:100|max:10000',
+            'author_id' => 'required|exists:users,id',
+            'category_id' => 'required|exists:categories,id',
+            'media' => 'required|file|mimes:jpeg,png,jpg,svg,mp4,mov,avi|max:10240',
+        ];
+    }
+
+    /**
+     * Customize the validation error messages.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'عنوان مقاله الزامی است.',
+            'title.min' => 'عنوان باید حداقل ۲۰ کاراکتر باشد.',
+            'description.required' => 'توضیحات الزامی است.',
+            'description.min' => 'توضیحات باید حداقل ۵۰ کاراکتر باشد.',
+            'text.required' => 'متن مقاله الزامی است.',
+            'text.min' => 'متن باید حداقل ۱۰۰ کاراکتر باشد.',
+            'author_id.required' => 'شناسه نویسنده الزامی است.',
+            'category_id.required' => 'شناسه دسته‌بندی الزامی است.',
+            'media.required' => 'فایل رسانه‌ای الزامی است.',
+            'media.mimes' => 'فایل رسانه‌ای باید از نوع jpeg، png، jpg، svg، mp4، mov، یا avi باشد.',
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'views' => $this->views ?: 0, // مقدار پیش‌فرض برای views
+        ]);
+    }
+}
