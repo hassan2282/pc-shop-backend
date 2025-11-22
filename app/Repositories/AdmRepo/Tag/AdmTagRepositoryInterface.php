6dd20e4ff8281f7be4a4305b9906b895a0595fc3
hassan2282 <taghavey.hassan@gmail.com>
2025-11-23 02:33:04 +0330

    add tag repository interface

---
+++ app/Repositories/AdmRepo/Tag/AdmTagRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\AdmRepo\Tag;
+
+use App\Repositories\EloquentRepositoryInterface;
+
+interface AdmTagRepositoryInterface extends EloquentRepositoryInterface
+{
+
+}
