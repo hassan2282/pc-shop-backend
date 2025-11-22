50b97441649a539bd3eb1981e100c01dc56ea753
hassan2282 <taghavey.hassan@gmail.com>
2025-11-22 23:39:36 +0330

    add update Product Request

---
+++ app/Http/Requests/Admin/Product/UpdateProductRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Admin\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'nullable|string|max:150|min:5',
+            'amount' => 'nullable|numeric',
+            'description' => 'nullable|string|min:20|max:500',
+            'text' => 'nullable|string|min:100|max:10000',
+            'category_id' => 'nullable|exists:categories',
+            'media' => 'nullable|file|jpeg,png,jpg,svg,mp4,mov,avi|max:10240',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.string' => 'نوع عنوان اشتباه است',
+            'title.min' => 'عنوان باید حداقل 5 کاراکتر باشد.',
+            'description.min' => 'توضیحات باید حداقل 20 کاراکتر باشد.',
+            'text.min' => 'متن باید حداقل ۱۰۰ کاراکتر باشد.',
+            'media.mimes' => 'فایل رسانه‌ای باید از نوع jpeg، png، jpg، svg، mp4، mov، یا avi باشد.',
+        ];
+    }
+}
