5f2a26c8608d81d31aad51b5d2e34bb2126c832d
hassan2282 <taghavey.hassan@gmail.com>
2025-11-23 02:33:50 +0330

    edit category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -23,4 +23,9 @@ class Category extends Model
     {
         return $this->hasMany(Article::class);
     }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
