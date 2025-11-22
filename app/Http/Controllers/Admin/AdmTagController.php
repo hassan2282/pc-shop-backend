daeb0af8aec07a9621a16753e44044dfa3ab86fd
hassan2282 <taghavey.hassan@gmail.com>
2025-11-23 02:32:38 +0330

    add tag controller

---
+++ app/Http/Controllers/Admin/AdmTagController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Services\AdmServices\AdmTagService;
+
+class AdmTagController extends Controller
+{
+    public function __construct(readonly protected AdmTagService $service)
+    {
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return $this->service->index();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store()
+    {
+        return $this->service->store();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return $this->service->show($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(int $id)
+    {
+        return $this->service->update($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return $this->service->delete($id);
+    }
+}
