ea236716d4338a811329f7fd4934dc83537f03a4
hassan2282 <taghavey.hassan@gmail.com>
2025-11-26 18:02:45 +0330

    add status and role_id to user faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -29,6 +30,8 @@ class UserFactory extends Factory
             'last_name' => fake()->lastName(),
             'phone' => fake()->unique()->phoneNumber(),
             'email' => fake()->unique()->safeEmail(),
+            'status' => fake()->boolean(),
+            'role_id' => Role::all()->random()->id,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
