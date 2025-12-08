5d2102cf41aba296e8050254d2e0fca2831a3d43
hassan2282 <taghavey.hassan@gmail.com>
2025-12-08 19:29:49 +0330

    add storeWithPivot to role create

--- app/Services/AdmServices/AdmRoleService.php
+++ app/Services/AdmServices/AdmRoleService.php
@@ -3,12 +3,35 @@
 namespace App\Services\AdmServices;
 use App\Http\Requests\Admin\Role\StoreRoleRequest;
 use App\Http\Requests\Admin\Role\UpdateRoleRequest;
+use App\Models\Role;
 use App\Repositories\AdmRepo\Role\AdmRoleRepositoryInterface;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class AdmRoleService extends BaseService
 {
+
     public function __construct(AdmRoleRepositoryInterface $repository)
     {
         parent::__construct($repository, StoreRoleRequest::class, UpdateRoleRequest::class);
     }
+
+
+    public function storeWithPivot(Request $request) {
+        try{
+
+            $role =  Role::create([
+                'name' => $request->name, 
+            ]);
+            $attach = $role->permissions()->attach($request->permissions);
+
+            if($role && $attach){
+                return response()->json('با موفقیت ساخته شد');
+            }
+
+        }catch(\Exception $e){
+            return response()->json($e->getMessage());
+        }
+    }
+    
 }
