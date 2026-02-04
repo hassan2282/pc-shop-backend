06a290fca43d6d9cf1dc963fc4588ef81ab8b3ec
hassan2282 <taghavey.hassan@gmail.com>
2026-02-04 14:11:17 +0330

    update code

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,14 +2,30 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasTags;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
     /** @use HasFactory<\Database\Factories\ArticleFactory> */
     use HasFactory;
+    use HasTags;
+    protected $guarded = ['id'];
+
+
+    protected static function booted()
+    {
+        static::creating(function ($article) {
+            $slug = Str::slug($article->title);
+            $count = Article::where('slug', 'like', "{$slug}%")->count();
+
+            $article->slug = $count ? "{$slug}-{$count}" : $slug;
+        });
+    }
+
 
     public function category(): BelongsTo
     {
