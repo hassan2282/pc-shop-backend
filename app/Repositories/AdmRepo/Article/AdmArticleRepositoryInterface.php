91ed8aebba6802c60e301f0322a280bf59d15121
hassan2282 <taghavey.hassan@gmail.com>
2026-02-06 01:26:52 +0330

    code updated

--- app/Repositories/AdmRepo/Article/AdmArticleRepositoryInterface.php
+++ app/Repositories/AdmRepo/Article/AdmArticleRepositoryInterface.php
@@ -6,5 +6,6 @@ use App\Repositories\EloquentRepositoryInterface;
 
 interface AdmArticleRepositoryInterface extends EloquentRepositoryInterface
 {
-
+     public function articlesWithRelation();
+     public function showWithRelation(int $id);
 }
