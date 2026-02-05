52e73b176dbe6f95e88df3be12ca75d8f3c204cd
hassan2282 <taghavey.hassan@gmail.com>
2026-02-06 01:26:41 +0330

    code updated

---
+++ app/Models/Traits/HasTags.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Traits;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+trait HasTags 
+{
+    public function tags(): MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+}
