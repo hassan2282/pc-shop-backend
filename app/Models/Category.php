ba0bfcdb510b662e31d014bde3c5dbb4f37fed48
hassan2282 <taghavey.hassan@gmail.com>
2026-02-06 01:26:22 +0330

    code updated

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -13,6 +14,19 @@ class Category extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            $slug = Str::slug($category->name);
+            $count = Category::where('slug', 'like', "{$slug}%")->count();
+
+            $category->slug = $count ? "{$slug}-{$count}" : $slug;
+        });
+    }
+
+
 
     public function parent(): BelongsTo
     {
