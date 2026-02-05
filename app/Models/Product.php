7f6db2dab0f46ae92d91a08e63c48680c616daae
hassan2282 <taghavey.hassan@gmail.com>
2026-02-06 01:26:31 +0330

    code updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,25 +2,36 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasTags;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+    use HasTags;
+
+
+    protected static function booted()
+    {
+        static::creating(function ($product) {
+            $slug = Str::slug($product->title);
+            $count = Product::where('slug', 'like', "{$slug}%")->count();
+
+            $product->slug = $count ? "{$slug}-{$count}" : $slug;
+        });
+    }
+
 
     protected $guarded = ['id'];
 
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'taggable');
     }
 
-    public function tags(): MorphToMany
-    {
-        return $this->morphToMany(Tag::class,'taggable');
-    }
 }
