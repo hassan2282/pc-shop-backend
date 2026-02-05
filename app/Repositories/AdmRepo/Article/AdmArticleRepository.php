0ad55abbd0c4dad82fd3cd236866ca70e2a782fa
hassan2282 <taghavey.hassan@gmail.com>
2026-02-06 01:26:49 +0330

    code updated

--- app/Repositories/AdmRepo/Article/AdmArticleRepository.php
+++ app/Repositories/AdmRepo/Article/AdmArticleRepository.php
@@ -4,7 +4,6 @@ namespace App\Repositories\AdmRepo\Article;
 
 use App\Models\Article;
 use App\Repositories\BaseRepository;
-use Illuminate\Database\Eloquent\Model;
 
 class AdmArticleRepository extends BaseRepository implements AdmArticleRepositoryInterface
 {
@@ -12,4 +11,33 @@ class AdmArticleRepository extends BaseRepository implements AdmArticleRepositor
     {
         parent::__construct($article);
     }
+
+
+
+    public function articlesWithRelation()
+    {
+        return Article::query()
+        ->select(['id','title','views','status','author_id','category_id'])
+        ->with([
+            'media:id,name,mediable_id,mediable_type',
+            'category:id,name',
+            'user:id,first_name,last_name',
+        ])
+        ->latest()
+        ->get();
+    }
+
+
+
+
+    public function showWithRelation(int $id)
+    {
+        return Article::select(['id','description','text','title','category_id'])
+        ->with([
+            'media:id,name,mediable_id,mediable_type',
+            'tags:id,name'
+        ])
+        ->find($id);
+    }
+
 }
