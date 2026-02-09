55f76ce9918f2226b027ead859de69979e1be9b6
hassan2282 <taghavey.hassan@gmail.com>
2026-02-09 18:01:21 +0330

    test new stuff

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,13 +8,16 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
+// $app->configure('cors');
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(HandleCors::class);
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
         $middleware->alias([
             'admin' => AdminMiddelware::class,
