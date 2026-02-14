f61f02599f07ee49ec33f75dc560c8635d100d31
hassan2282 <taghavey.hassan@gmail.com>
2026-02-14 23:36:22 +0330

    add relations to product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,9 @@ use App\Models\Traits\HasTags;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Support\Str;
 
@@ -14,6 +17,7 @@ class Product extends Model
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
     use HasTags;
+    protected $guarded = ['id'];
 
 
     protected static function booted()
@@ -27,11 +31,24 @@ class Product extends Model
     }
 
 
-    protected $guarded = ['id'];
-
     public function category(): BelongsTo
     {
-        return $this->belongsTo(Category::class, 'taggable');
+        return $this->belongsTo(Category::class);
+    }
+
+    public function attribute_values(): BelongsToMany
+    {
+        return $this->belongsToMany(Attribute_value::class);
+    }
+
+    public function tags(): MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'mediable');
     }
 
 }
