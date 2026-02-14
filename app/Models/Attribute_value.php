a26cafab6c6af8b861d7fe00fd23755862eee1f5
hassan2282 <taghavey.hassan@gmail.com>
2026-02-14 23:35:23 +0330

    add attribute_value model

---
+++ app/Models/Attribute_value.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Attribute_value extends Model
+{
+    /** @use HasFactory<\Database\Factories\AttributeValueFactory> */
+    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function attribute(): BelongsTo
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
