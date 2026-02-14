3307410fafdfceb5db02645dd2a7f3c5668ae65d
hassan2282 <taghavey.hassan@gmail.com>
2026-02-14 23:37:51 +0330

    add attribute_value repository interface

---
+++ app/Repositories/AdmRepo/Attribute_value/AdmAttribute_valueRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\AdmRepo\Attribute_value;
+
+use App\Repositories\EloquentRepositoryInterface;
+
+interface AdmAttribute_valueRepositoryInterface extends EloquentRepositoryInterface
+{
+
+}
