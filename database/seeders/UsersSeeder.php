eceab0e8e0a44f0d3afce74ef5e9b5ec756e99df
hassan2282 <taghavey.hassan@gmail.com>
2026-02-15 18:20:25 +0330

    add admin user seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'username' => 'photoshopbaz98',
+            'first_name' => 'سید حسن',
+            'last_name' => 'تقوی',
+            'phone' => '09170249855',
+            'email' => 'taghavey.hassan@gmail.com',
+            'status' => true,
+            'role_id' => 2,
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456789'),
+            'remember_token' => Str::random(10),
+        ]);
         User::factory()->count(20)->create();
     }
 }
