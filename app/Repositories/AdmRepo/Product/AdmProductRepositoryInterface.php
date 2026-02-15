195c27d794e6919216ccbb742eb22deb10c038cc
hassan2282 <taghavey.hassan@gmail.com>
2026-02-15 18:12:52 +0330

    add 2 new method to home page

--- app/Repositories/AdmRepo/Product/AdmProductRepositoryInterface.php
+++ app/Repositories/AdmRepo/Product/AdmProductRepositoryInterface.php
@@ -2,9 +2,12 @@
 
 namespace App\Repositories\AdmRepo\Product;
 
+use App\Models\Product;
 use App\Repositories\EloquentRepositoryInterface;
 
 interface AdmProductRepositoryInterface extends EloquentRepositoryInterface
 {
-
+    public function productWithRelations();
+    public function productsForHome();
+    public function showProduct(Product $product);
 }
