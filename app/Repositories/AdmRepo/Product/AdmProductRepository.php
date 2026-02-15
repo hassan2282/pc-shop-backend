07d12aa6f0ba95c1e0f000d53dd9a2a80b625c06
hassan2282 <taghavey.hassan@gmail.com>
2026-02-15 18:12:34 +0330

    add product with relations

--- app/Repositories/AdmRepo/Product/AdmProductRepository.php
+++ app/Repositories/AdmRepo/Product/AdmProductRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories\AdmRepo\Product;
 
 use App\Models\Product;
 use App\Repositories\BaseRepository;
-use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 class AdmProductRepository extends BaseRepository implements AdmProductRepositoryInterface
 {
@@ -12,4 +12,45 @@ class AdmProductRepository extends BaseRepository implements AdmProductRepositor
     {
         parent::__construct($model);
     }
+    
+    public function productWithRelations()
+    {
+        return $this->model->with([
+            'media' => fn($query) => $query->limit(1),
+            'category:id,name',
+        ])
+        ->orderBy('id','DESC')
+        ->get() ;
+    }
+
+
+    public function productsForHome()
+    {
+        return $this->model->with([
+            'media' => fn($query) => $query->limit(1),
+            'category:id,name',
+            'attribute_values.attribute'
+        ])
+        ->orderBy('id','DESC')
+        ->get() ;
+    }
+
+
+
+
+
+
+    public function showProduct(Product $product)
+    {
+        
+         $data = $product->load([
+                'media',
+                'tags:id,name',
+                'attribute_values.attribute',
+            ]);
+        
+
+        return response()->json($data, HttpResponse::HTTP_OK);
+    }
+
 }
