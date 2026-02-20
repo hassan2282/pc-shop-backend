9e5b748cb24438cb402c387676c2f114bfbe7e36
hassan2282 <taghavey.hassan@gmail.com>
2026-02-20 20:28:28 +0330

    add ticket controller

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserTicketStoreRequest;
+use App\Services\TicketService;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+
+    public function __construct(readonly protected TicketService $service) {}
+
+
+    public function show(int $id)
+    {
+        return $this->service->show($id);
+    }
+
+
+    public function store(UserTicketStoreRequest $request)
+    {
+        return $this->service->store($request->toArray());
+    }
+
+
+    public function destroy($id)
+    {
+        return $this->service->destroy($id);
+    }
+}
