b006bc7e030de82acd3d7bfefea41a5bddb676d4
hassan2282 <taghavey.hassan@gmail.com>
2026-04-21 17:21:15 +0330

    send ticket notifs to admins

--- app/Services/AdmServices/AdmTicketService.php
+++ app/Services/AdmServices/AdmTicketService.php
@@ -4,6 +4,8 @@ namespace App\Services\AdmServices;
 
 use App\Http\Requests\Admin\Ticket\StoreTicketRequest;
 use App\Http\Requests\Admin\Ticket\UpdateTicketRequest;
+use App\Models\User;
+use App\Notifications\TicketNotification;
 use App\Repositories\AdmRepo\Ticket\AdmTicketRepositoryInterface;
 
 
@@ -17,6 +19,10 @@ class AdmTicketService extends BaseService
     public function storeAndReturnTicket(StoreTicketRequest $request)
     {
         $data =  $this->repository->create($request->toArray());
+        if($data){
+            $targetUser = User::find($data->conversation->user_id);
+            $targetUser->notify(new TicketNotification($data));
+        }
         return response()->json($data);
     }
 
