2a0c7afa8dabd3d46273d8e4c071452d722b9217
hassan2282 <taghavey.hassan@gmail.com>
2026-04-21 17:17:55 +0330

    add notification controller

---
+++ app/Http/Controllers/Admin/AdmNotificationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class AdmNotificationController extends Controller
+{
+    public function all()
+{
+    try {
+        $admin = User::where('role_id', '>', 1)->firstOrFail();
+        $notifications = $admin->notifications()->paginate(10);
+        
+        return response()->json([
+            'total' => $notifications->total() / 10,
+            'data'  => $notifications->items(),
+        ], 200);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    };
+}
+
+
+    public function show()
+    {
+        
+    }
+}
