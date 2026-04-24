18b03405beb4171f4e3f88abb2b1263ce27c4a71
hassan2282 <taghavey.hassan@gmail.com>
2026-04-24 16:29:47 +0330

    create article notification

---
+++ app/Notifications/CreateArticleNotification.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CreateArticleNotification extends Notification
+{
+    use Queueable;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toDatabase(object $notifiable)
+    {
+        return [
+            'title' => 'مقاله جدید',
+            'body' => 'مقاله جدید به لیست مقالات افزوده شد',
+            'url' => '/admin/article/all',
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
